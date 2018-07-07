2899dfcdd5e868f071e84e2bfbbe0f2230b35807
Wuk <wuke_1024@sina.com>
2018-07-07 18:05:02 +0800

    删除一些注释掉的废代码

--- data.js
+++ data.js
@@ -5,28 +5,6 @@
 /*
 var lng = 113.364805;
 var lat = 23.140929;
-
-
-var coordData = [];
-for (var i = 0; i < 200; i++) {
-    coordData.push([lng + Math.random() / 1000, lat + Math.random() / 1000]);
-    coordData.push([lng + Math.random() / 1000, lat - Math.random() / 1000]);
-    coordData.push([lng - Math.random() / 1000, lat - Math.random() / 1000]);
-    coordData.push([lng - Math.random() / 1000, lat + Math.random() / 1000]);
-}
-
-var gridArr = [];// minLng, minLat, maxLng, maxLat
-var opt = {};
-var gridData = [];
-for (var i = 0; i < 100; i++) {
-    gridArr[0] = lng - Math.random() / 1000;// minLng
-    gridArr[1] = lat + Math.random() / 1000;// minLat
-    gridArr[2] = lng + Math.random() / 1000;// maxLng
-    gridArr[3] = lat - Math.random() / 1000;// maxLat
-    opt.fillStyle = "rgba(50, 50, 255, 0.3 )";
-    opt.strokeStyle = "rgba(50, 50, 255, 0.7)";
-    gridData.push({ gridArr, opt });
-}
 */
 var max = { lng: 113.375905, lat: 23.152029 };
 var min = { lng: 113.353705, lat: 23.129829 };
